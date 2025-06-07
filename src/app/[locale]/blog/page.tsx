3e1a854b0da0c4d7785686c06682da4317eb47fb
Anush Chakhoyan <anush.chakhoyan@inspace.chat>
2025-06-07 23:57:22 +0400

    build(): update folders

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,66 +1,66 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import type { FC } from 'react';
 import { useTranslations } from "next-intl";
 
 // Sample data
-const items = [
-    {
-        id: 1,
-        name: 'Item a',
-        language: 'en',
-        title: 'Taking Your React Styling to the Next Level with Styled Components',
-        description: 'The Styled Components is a modern way to style React components because it allows you to write styles in a declarative and modular way…',
-        date: 'Published on May 9, 2023',
-        reading: '7 min read',
-        src: ''
-    },
-    {
-        id: 2,
-        name: 'Item b',
-        language: 'en',
-        title: 'How the Browser Renders a Web Page — CSSOM, Render Engine',
-        description: 'Understanding what goes on inside a browser is the most powerful part for every web developer.We’ll look at how browsers interpret code…',
-        date: 'Published on May 26, 2023',
-        reading: '6 min read',
-        src: ''
-    },
-    {
-        id: 3,
-        name: 'Item c',
-        language: 'en',
-        title: 'Ձեր վեբ կայքը ավելի ինտերակտիվ դարձնելու ժամանակն է. Սովորե՛ք CSS Focus Selector-ները հիմա',
-        description: 'Understanding what goes on inside a browser is the most powerful part for every web developer.We’ll look at how browsers interpret code…',
-        date: 'Published on Jul 3, 2023',
-        reading: '2 min read',
-        src: ''
-    },
-    {
-        id: 4,
-        name: 'Item d',
-        language: 'en',
-        title: 'How the Browser Renders a Web Page — CSSOM, Render Engine',
-        description: 'Understanding what goes on inside a browser is the most powerful part for every web developer.We’ll look at how browsers interpret code…',
-        date: 'Published on May 26, 2023',
-        reading: '6 min read',
-        src: ''
-    },
-];
+// const items = [
+//     {
+//         id: 1,
+//         name: 'Item a',
+//         language: 'en',
+//         title: 'Taking Your React Styling to the Next Level with Styled Components',
+//         description: 'The Styled Components is a modern way to style React components because it allows you to write styles in a declarative and modular way…',
+//         date: 'Published on May 9, 2023',
+//         reading: '7 min read',
+//         src: ''
+//     },
+//     {
+//         id: 2,
+//         name: 'Item b',
+//         language: 'en',
+//         title: 'How the Browser Renders a Web Page — CSSOM, Render Engine',
+//         description: 'Understanding what goes on inside a browser is the most powerful part for every web developer.We’ll look at how browsers interpret code…',
+//         date: 'Published on May 26, 2023',
+//         reading: '6 min read',
+//         src: ''
+//     },
+//     {
+//         id: 3,
+//         name: 'Item c',
+//         language: 'en',
+//         title: 'Ձեր վեբ կայքը ավելի ինտերակտիվ դարձնելու ժամանակն է. Սովորե՛ք CSS Focus Selector-ները հիմա',
+//         description: 'Understanding what goes on inside a browser is the most powerful part for every web developer.We’ll look at how browsers interpret code…',
+//         date: 'Published on Jul 3, 2023',
+//         reading: '2 min read',
+//         src: ''
+//     },
+//     {
+//         id: 4,
+//         name: 'Item d',
+//         language: 'en',
+//         title: 'How the Browser Renders a Web Page — CSSOM, Render Engine',
+//         description: 'Understanding what goes on inside a browser is the most powerful part for every web developer.We’ll look at how browsers interpret code…',
+//         date: 'Published on May 26, 2023',
+//         reading: '6 min read',
+//         src: ''
+//     },
+// ];
 
-type SortOption = 'name' | 'language';
+// type SortOption = 'name' | 'language';
 
 const Blog: FC = () => {
     const t = useTranslations("Blog");
-    const [sortBy, setSortBy] = useState<SortOption>('name');
+    // const [sortBy, setSortBy] = useState<SortOption>('name');
 
     // Sort items based on selected sort option
-    const sortedItems = [...items].sort((a, b) => {
-        if (sortBy === 'name') {
-            return a.name.localeCompare(b.name);
-        } else {
-            return a.language.localeCompare(b.language);
-        }
-    });
+    // const sortedItems = [...items].sort((a, b) => {
+    //     if (sortBy === 'name') {
+    //         return a.name.localeCompare(b.name);
+    //     } else {
+    //         return a.language.localeCompare(b.language);
+    //     }
+    // });
 
     return (
         <div className="w-full max-w-4xl mx-auto py-8">
